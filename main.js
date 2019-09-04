193d8a1b128ae38b0c76a733b0814fcd5563fc0b
tricker <ogino.ktai@gmail.com>
2019-09-04 23:41:46 +0900

    add Road01 - add road(canvas) in html - infinite movement x directive

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = main;
 
-var ctx;
+var ctx;        //global  canvasのcontext
 var last_time;
 var x;
 var car1;
@@ -49,30 +49,81 @@ class car{
         context.fill();
     }
 }
+/* car : only move x directive */
+class car_x{
+    constructor(x_,y_,color_,){
+        this.x = x_;
+        this.y = y_;            //ground position
+        this.color = color_;
+        this.draw_self();
+        this.bgcolor = "gray";
 
+        /* 世界の情報 */
+        this.world_width = 800; //px
+        /* 前回値更新 */
+        this.x0 = this.x;
+    }
+
+    move(x_move){
+        /* 世界の大きさを考慮 */    //[TODO]値のsetterにも考慮させるべきか
+        this.x = this.x + x_move;
+        if(this.x > this.world_width){
+            this.x = (this.x + x_move)%this.world_width;
+        }else if(this.x < 0){
+            this.x = this.world_width - this.x;
+        }
+
+        this.delete_oldme();
+        this.draw_self();
+
+        /* 前回値更新 */
+        this.x0 = this.x;
+    }
+    draw_self(){
+        ctx.beginPath();
+        ctx.fillStyle = (this.color);
+        ctx.fillRect(this.x,this.y,4,4);
+    }
+    delete_oldme(){
+        ctx.beginPath();
+        ctx.fillStyle = (this.bgcolor);
+        ctx.fillRect(this.x0,this.y,4,4);
+    }
+}
 function main(){
-var canvas = document.getElementById("field");
-canvas.innerText = "test";
+    ctx = document.getElementById("field").getContext('2d');
 
-ctx = canvas.getContext('2d');
+    car1 = new car(10,40);
+    car2 = new car(300,200);
 
-car1 = new car(10,40);
-car2 = new car(300,200);
+    /* Road01 */
+    ctx = document.getElementById("road01").getContext('2d');
+    car3 = new car_x(50,80,"black",ctx);
+    car3.bgcolor = "blanchedalmond";
 
-window.requestAnimationFrame(()=>loop_animation());
+    window.requestAnimationFrame(()=>loop_animation());
 }
 
 function loop_animation(){
-/* 経過時間計算 */
-var now_time = performance.now();
-var delta_time = now_time - last_time;
-last_time = now_time;
-var delta_x = delta_time/60;
+    /* 経過時間計算 */
+    var now_time = performance.now();
+    var delta_time = now_time - last_time;
+    last_time = now_time;
+    var delta_x = delta_time/60;
 
+    /* field */
+    ctx = document.getElementById("field").getContext('2d');
     ctx.fillRect(x,20,4,4);
     x = x+1;
 
     car1.heading_target(ctx,car2);
     car2.escape_enemy(ctx,car1);
+
+    /* Road01 */
+    ctx = document.getElementById("road01").getContext('2d');
+    car3.move(1);
+
+
+    /* アニメーションループ */
     window.requestAnimationFrame((x)=>loop_animation(x));
 }
