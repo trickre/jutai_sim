2545904ae2f3b0d798bc614878b7e056142fb2ca
tricker <ogino.ktai@gmail.com>
2019-09-04 22:54:58 +0900

    rect to circle, delete magnet class

--- main.js
+++ main.js
@@ -10,14 +10,17 @@ class car{
     constructor(x_,y_){
         this.x = x_;
         this.y = y_;
+        this.color = "black";
         //自身を描画
     }
 
     randomove(context){
         this.x += (Math.random()-0.5)*2;
         this.y += (Math.random()-0.5)*2;
-
-        context.fillRect(this.x,this.y,4,4);
+        context.beginPath();
+        context.arc(this.x,this.y,2,0,Math.PI*2,true);
+        context.fillStyle = ("green");
+        context.fill();
     }
 
     heading_target(context,target_car){
@@ -27,7 +30,10 @@ class car{
 
         this.x += dir_x/denominator + (Math.random()-0.5)*2;
         this.y += dir_y/denominator + (Math.random()-0.5)*2;
-        context.fillRect(this.x,this.y,4,4);
+        context.beginPath()
+        context.arc(this.x,this.y,2,0,Math.PI*2,true);
+        context.fillStyle = (this.color);
+        context.fill();
     }
 
     escape_enemy(context,enemy_car){
@@ -37,26 +43,10 @@ class car{
 
         this.x += dir_x/denominator +(Math.random()-0.5)*2;
         this.y += dir_y/denominator +(Math.random()-0.5)*2;
-        context.fillRect(this.x,this.y,4,4);
-    }
-}
-
-class magnet{
-    constructor(context,x_,y_){
-        this.x = x_;
-        this.y = y_;
-        //自身を描画
-        context.fillRect(this.x,this.y,5,5);//center:(x+3,y+3)
-    }
-
-    pull_car(context,car){
-        var dir_x = this.x+3 - car.x;
-        var dir_y = this.y+3 - car.y;
-        var denominator = Math.sqrt(dir_x**2+dir_y**2);
-
-        car.x  += dir_x/denominator*0.5;
-        car.y  += dir_y/denominator*0.5;
-        context.fillRect(car.x,car.y,4,4);
+        context.beginPath()
+        context.arc(this.x,this.y,2,0,Math.PI*2,true);
+        context.fillStyle = (this.color);
+        context.fill();
     }
 }
 
@@ -68,7 +58,6 @@ ctx = canvas.getContext('2d');
 
 car1 = new car(10,40);
 car2 = new car(300,200);
-mag1 = new magnet(ctx,200,200);
 
 window.requestAnimationFrame(()=>loop_animation());
 }
@@ -83,10 +72,7 @@ var delta_x = delta_time/60;
     ctx.fillRect(x,20,4,4);
     x = x+1;
 
-    //car2.randomove(ctx);
-    //car1.randomove(ctx);
     car1.heading_target(ctx,car2);
     car2.escape_enemy(ctx,car1);
-    mag1.pull_car(ctx,car2);
     window.requestAnimationFrame((x)=>loop_animation(x));
 }
